d615e040221983fd538a968cd5b196f54ad4b283
youjeong2 <feb_green@naver.com>
2020-08-07 21:00:21 +0900

    Vue-CLI Todolist

--- frontend/src/components/TodoList.vue
+++ frontend/src/components/TodoList.vue
@@ -1,30 +1,59 @@
 <template>
   <div>
     <h3> Todo List</h3>
-    <ul>
-      <li v-for="(todoItem, idx) in todoItems"
-          v-bind:key="idx">
-          {{ todoItem }}
-          <button v-on:click="removeTodo(todoItem, idx)">Delete</button>
-      </li>
-    </ul>
+      <ul>
+        <h3>Todo List</h3>
+        // bind는 데이터, on은 mehthod
+        // editingId를 "editingId" 로 mapping?
+        <todo-item v-for="todoItem in todoItems"
+          v-bind:key="todoItem.id"
+          v-bind:todoItem="todoItem"
+          v-bind:editingId="editingId"
+          v-on:removeTodo="onRemoveTodo"
+          v-on:updateTodo="onUpdateTodo"
+          v-on:setEditingId="SET_EDITING_ID"
+          v-on:resetEditingId="RESET_EDITING_ID"
+          v-on:toggleTodoStatus="onToggleTodoStatus"/>
+      </ul>
   </div>
 </template>
 
 <script>
-
+import TodoItem from './TodoItem.vue'
+import { mapState, mapMutations, mapGetters } from 'vuex'
+import { RESET_EDITING_ID, SET_EDITING_ID } from '../store/mutation-types'
 export default {
-  data () {
-    return {
-    // todoItems: ['item1', 'item2', 'item3']
-    }
+  // 컴포넌트 TodoItem추가
+  components: {
+    'todo-item': TodoItem
   },
-  props: ['todoItems'],
+  // 단순계산
+  computed: {
+    todoItems () {
+      return this.filteredTodoItems
+    },
+    ...mapState([
+      'editingId'
+    ]),
+    ...mapGetters([
+      'filteredTodoItems'
+    ])
+  },
+  // 상위에 있는 removeTodo에서 "onRemoveTodo"가 호출되니까
+  // onRemove를 methods에 추가시켜줌
   methods: {
-    removeTodo (todoItem, idx) {
-      console.log('removeTodo')
-      // this.todoItems.splice(idx, 1)
-      this.$emit('removeTodo', todoItem, idx)
+  ...mapMutations([
+    SET_EDITING_ID,
+    RESET_EDITING_ID
+  ]),
+  onRemoveTodo (id) {
+    this.$emit('removeTodo', id)
+  },
+  onUpdateTodo (id) {
+    this.$emit('updateTodo', id)
+  },
+  onToggleTodoStatus (id) {
+    this.$emit('toggleTodoStatus')
     }
   }
 }
